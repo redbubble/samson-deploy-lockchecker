f19920ba2f6b57a749cf039636c88dc309c461e0
yufei.chen <yufei.chen@redbubble.com>
2016-10-03 12:59:14 +1100

    add missing namespace

--- lib/samson_deploy_lockchecker/samson_plugin.rb
+++ lib/samson_deploy_lockchecker/samson_plugin.rb
@@ -5,6 +5,5 @@ end
 
 
 Samson::Hooks.callback :release_deploy_conditions do |stage, release|
-
-  Lockchecker.new.run_auto_deploy?(release, stage)
+  SamsonDeployLockchecker::Lockchecker.new.run_auto_deploy?(release, stage)
 end
